5d69c859d0f6ab6612f54369bc9727534144e721
James Trenholme <jje.trenholme@gmail.com>
2020-03-17 22:01:34 +0000

    added image fade animation and subtitle for work page. Also made a few style tweeks to generic sections etc

--- src/views/Work.vue
+++ src/views/Work.vue
@@ -4,8 +4,18 @@
       <section class="section section--full flex--column">
         <div class="content-block content-block--center">
           <div class="title text-block">
-            <h1>Asda</h1>
+            <h1>Web Designer</h1>
           </div>
+          <div class="flex" style="">
+            <div class="subtitle text-block margin-right">
+              <h1>ASDA</h1>
+            </div>
+            <div class="subtitle text-block">
+              <h1 class="date">Mar 2016 - Oct 2017</h1>
+            </div>
+          </div>
+        </div>
+        <div class="content-block">
           <p>
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi doloribus quam aliquam minus nihil ad enim provident fugit dignissimos id tenetur reiciendis natus assumenda veritatis, officia dolor rerum aut eaque.
           </p>
@@ -20,20 +30,21 @@
             <li class="content-block__list-item"><p>Illustrator</p></li>
           </ul>
         </div>
-        <div class="content-block">
+        <!-- <div class="content-block">
           <span class="chevron chevron--down flex--content-center"></span>
-        </div>
+        </div> -->
+        <hr class="hide-desktop" />
       </section>
       <section class="section section--full flex--column">
         <div class="content-block content-block--full content-block--center">
           <ul class="content-block__list">
-            <li class="content-block__list-item content-block__list-img"><img src="https://trenholme90.github.io/images/Toni&Guy-screenshot.jpg" /></li>
+            <li class="content-block__list-item content-block__list-img fade-animation"><img src="https://trenholme90.github.io/images/Toni&Guy-screenshot.jpg" /></li>
             <!-- <li class="content-block__list-item content-block__list-img"><img src="https://trenholme90.github.io/images/Bacardi-screenshot.jpg" /></li> -->
           </ul>
         </div>
       </section>
     </div>
-    <div class="section--hero__img asda-img"></div>
+    <div class="section--hero__img app-background-image asda-img"></div>
   </div>
 </template>
 
