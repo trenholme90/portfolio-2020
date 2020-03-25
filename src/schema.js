3b08c371e4cd32b39d687a3b395808ee51998d0f
James Trenholme <jje.trenholme@gmail.com>
2020-03-25 11:48:06 +0000

    Added dynamic content driven by data for each place of work

---
+++ src/schema.js
@@ -0,0 +1,41 @@
+export default
+{
+    "item1": {
+        "page": "Jet2",
+        "role": "Front-end Developer",
+        "date": "Oct 2017 - Present",
+        "text": "Creating components for both Jet2 and Jet2Holidays with HTML5, Sass and Vue.js in a .net Sitecore build. A big part of the role is working with the CRO (Conversion Rate Optimisation) team to test new functionality through A/B or MVT tests to improve conversion.",
+        "skills": ["HTML", "SASS (BEM methodology)", "Javascript", "Vue.js", "Optimizely", "Sitecore", "Git"],
+        "images": ["https://trenholme90.github.io/images/Toni&Guy-screenshot.jpg",
+                "https://trenholme90.github.io/images/Bacardi-screenshot.jpg",
+                "https://trenholme90.github.io/images/Toni&Guy-screenshot.jpg"]
+    },
+    "item2": {
+        "page": "ASDA",
+        "role": "Web Designer",
+        "date": "Feb 2016 - Oct 2017",
+        "text": "Using key web technologies, I created landing pages for branded advertisements on the groceries website (click through the gallery to see which brands). This is also alongside constructing and editing imagery for branded advertisements adn uploading them to the CMS.",
+        "skills": ["HTML", "CSS", "Javascript", "Photoshop", "Illustrator"],
+        "images": ["https://trenholme90.github.io/images/Toni&Guy-screenshot.jpg",
+                "https://trenholme90.github.io/images/Bacardi-screenshot.jpg"]
+
+    },
+    "item3": {
+        "page": "Github",
+        "role": "Front-end Developer",
+        "date": "Spare time",
+        "text": "A place to see my work outside of work. These are side projects that I was inspired to make and are a variety of different projects. Feel free to pull them down and take a look.",
+        "skills": ["Node.js", "Vue.js", "Web push", "API", "Firebase", "Sass", "Git", "JSON"],
+        "images": ["https://trenholme90.github.io/images/Toni&Guy-screenshot.jpg",
+                "https://trenholme90.github.io/images/Bacardi-screenshot.jpg"]
+    },
+    "item4": {
+        "page": "Loko Pet Apparel",
+        "role": "Freelance Developer",
+        "date": "Aug 2015 - Feb 2016",
+        "text": "A WordPress based eCommerce store for dog apparel. Created my own sub-theme from an existing theme and modified the front end HTML and styles to fit the breif.",
+        "skills": ["HTML", "CSS", "WordPress", "WooCommerce", "Javascript"],
+        "images": ["https://trenholme90.github.io/images/Toni&Guy-screenshot.jpg",
+                "https://trenholme90.github.io/images/Bacardi-screenshot.jpg"]
+    }
+}
--- src/views/Work.vue
+++ src/views/Work.vue
@@ -4,49 +4,33 @@
       <section class="section section--full flex--column">
         <div class="content-block content-block--center">
           <div class="title text-block">
-            <h1>Web Designer</h1>
+            <h1>{{ role }}</h1>
           </div>
           <div class="flex" style="">
             <div class="subtitle text-block margin-right">
-              <h1>ASDA</h1>
+              <h1>{{ place }}</h1>
             </div>
             <div class="subtitle text-block">
-              <h1 class="date">Mar 2016 - Oct 2017</h1>
+              <h1 class="date">{{ date }}</h1>
             </div>
           </div>
         </div>
         <div class="content-block">
           <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi doloribus quam aliquam minus nihil ad enim provident fugit dignissimos id tenetur reiciendis natus assumenda veritatis, officia dolor rerum aut eaque.
+            {{ text }}
           </p>
         </div>
         <div class="content-block content-block--full content-block--center">
           <h1>Skills</h1>
           <ul class="content-block__list">
-            <li class="content-block__list-item"><p>HTML</p></li>
-            <li class="content-block__list-item"><p>CSS</p></li>
-            <li class="content-block__list-item"><p>Javascript</p></li>
-            <li class="content-block__list-item"><p>PhotoShop</p></li>
-            <li class="content-block__list-item"><p>Illustrator</p></li>
+            <li v-for="skill in skills" :key="skill.index" class="content-block__list-item">{{ skill }}</li>
           </ul>
         </div>
-        <!-- <div class="content-block">
-          <span class="chevron chevron--down flex--content-center"></span>
-        </div> -->
         <hr class="hide-desktop" />
       </section>
       <section class="section section--full flex--column">
-        <!-- <div class="content-block content-block--full content-block--center">
-          <ul class="content-block__list">
-            <li class="content-block__list-item content-block__list-img fade-animation"><img src="https://trenholme90.github.io/images/Toni&Guy-screenshot.jpg" /></li>
-            <li class="content-block__list-item content-block__list-img"><img src="https://trenholme90.github.io/images/Bacardi-screenshot.jpg" /></li>
-          </ul>
-        </div> -->
         <Slider
-          :sliderImages="[
-            'https://trenholme90.github.io/images/Toni&Guy-screenshot.jpg',
-            'https://trenholme90.github.io/images/Bacardi-screenshot.jpg'
-          ]"
+          :sliderImages="images"
         />
       </section>
     </div>
@@ -56,10 +40,55 @@
 
 <script>
 import Slider from "../components/Slider.vue";
+import Schema from "../schema";
 export default {
   name: "work",
   components: {
     Slider
+  },
+  data: function() {
+    return {
+      place: "",
+      role: "",
+      date: "",
+      text: "",
+      skills: [],
+      images: []
+    }
+  },
+  beforeMount() {
+    this.dataHandler()
+  },
+  watch: {
+    $route (){
+      this.dataHandler()
+    }
+  },
+  methods: {
+    dataHandler: function() {
+      for (let item in Schema) {
+        if (Object.hasOwnProperty.call(Schema, item)) {
+          const pageName = Schema[item].page,
+                role = Schema[item].role,
+                date = Schema[item].date,
+                text = Schema[item].text,
+                skills = Schema[item].skills,
+                images = Schema[item].images,
+                currentRoute = this.$route.params.id;
+
+          if(pageName === currentRoute) {
+            this.place = pageName
+            this.role = role
+            this.date = date
+            this.text = text
+            this.skills = skills
+            this.images = images
+          }
+          console.log(this.skills)
+          console.log(this.images)
+        }
+      }
+    }
   }
 };
 </script>
